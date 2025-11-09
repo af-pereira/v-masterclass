9717a13250c9ec1ad785912bdf393d091206a1c5
Ana <ana.pereiraxc@gmail.com>
2025-11-09 11:09:05 +0000

    Add alias. Add dynamic routing by page file

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,38 +1,9 @@
-import HomeView from '@/views/HomeView.vue'
-import { createRouter, createWebHistory } from 'vue-router'
-import { h } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/projects',
-      name: 'projects',
-      component: () => import('@/views/ProjectsView.vue')
-    },
-    {
-      path: '/projects/:id',
-      name: 'single-project',
-      component: () => import('@/views/SingleProjectView.vue')
-    },
-    {
-      // Project not found
-      path: '/projects/:pathMatch(.*)*',
-      name: 'project-not-found',
-      component: () => h('p', { style: 'color:red;' }, '404 Project not found!')
-    },
-    {
-      // Catch all unmatched routes
-      path: '/:pathMatch(.*)*',
-      name: 'not-found',
-      component: () => h('p', { style: 'color:red;' }, '404 Not found!')
-    }
-  ]
+  routes
 })
 
 export default router
--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import VueRouter from 'unplugin-vue-router/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [VueRouter(), vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
@@ -18,6 +16,6 @@ export default defineConfig({
       '@stores': fileURLToPath(new URL('./src/stores', import.meta.url)),
       '@router': fileURLToPath(new URL('./src/router', import.meta.url)),
       '@assets': fileURLToPath(new URL('./src/assets', import.meta.url))
-    },
-  },
+    }
+  }
 })
