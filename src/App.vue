6e517c7c8638bb36c0602fd53f9e1f6e0644880f
Ana <ana.pereiraxc@gmail.com>
2025-11-09 18:41:52 +0000

    Add first shadcn components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,38 @@
 <script setup lang="ts">
-import { supabase } from '@lib/supabaseClient'
-
-console.log(supabase)
+import Input from '@/components/ui/input/Input.vue'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+import { Avatar, AvatarFallback } from '@/components/ui/avatar'
 </script>
 
 <template>
-  <RouterView />
+  <nav class="h-16 border-b bg-muted/40 flex justify-between px-6 items-center">
+    <form class="w-full max-w-96">
+      <Input class="w-full pl-8 bg-background" type="text" placeholder="Search..." />
+    </form>
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarFallback>AP</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Settings</DropdownMenuItem>
+        <DropdownMenuItem>Logout</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  </nav>
+
+  <main>
+    <RouterView />
+  </main>
 </template>
