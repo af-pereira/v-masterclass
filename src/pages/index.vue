e9619f871be1840d6bb08bceaa1f154d7008b95c
Ana <ana.pereiraxc@gmail.com>
2025-11-09 11:56:23 +0000

    Add typed routes

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,6 +3,6 @@
 <template>
   <div>
     <h1>Home View</h1>
-    <RouterLink to="/projects">Go to Projects</RouterLink>
+    <RouterLink :to="{ name: '/projects/' }">Go to Projects</RouterLink>
   </div>
 </template>
