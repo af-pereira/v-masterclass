786a30992906a5a492e82cb33238679d9456bc14
Ana <ana.pereiraxc@gmail.com>
2025-11-09 14:22:56 +0000

    Supabase implementation. warning

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,8 @@ export default defineConfig({
       '@views': fileURLToPath(new URL('./src/views', import.meta.url)),
       '@stores': fileURLToPath(new URL('./src/stores', import.meta.url)),
       '@router': fileURLToPath(new URL('./src/router', import.meta.url)),
-      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url))
+      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+      '@lib': fileURLToPath(new URL('./src/lib', import.meta.url))
     }
   }
 })
