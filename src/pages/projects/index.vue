743042a76df23112aafc01fea631bb55b02f2f5b
Ana <ana.pereiraxc@gmail.com>
2025-11-28 22:03:47 +0000

    Tidy up column definitions

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
-import { RouterLink } from 'vue-router'
 import { projectsQuery } from '@/utils/supaQueries'
-import type { ColumnDef } from '@tanstack/vue-table'
 import type { Projects } from '@/utils/supaQueries'
+import { columns } from '@/utils/tableColumns/projectsColumns'
 
 usePageStore().pageData.title = 'Projects'
 
@@ -18,41 +17,6 @@ const getProjects = async () => {
 }
 
 await getProjects()
-
-const columns: ColumnDef<Projects[0]>[] = [
-  {
-    accessorKey: 'name',
-    header: () => h('div', { class: 'text-left' }, 'Name'),
-    cell: ({ row }) => {
-      return h(
-        RouterLink,
-        {
-          to: `/projects/${row.original.slug}`,
-          class: 'text-left font-medium hover:bg-muted block w-full'
-        },
-        () => row.getValue('name')
-      )
-    }
-  },
-  {
-    accessorKey: 'status',
-    header: () => h('div', { class: 'text-left' }, 'Status'),
-    cell: ({ row }) => {
-      return h('div', { class: 'text-left font-medium' }, row.getValue('status'))
-    }
-  },
-  {
-    accessorKey: 'collaborators',
-    header: () => h('div', { class: 'text-left' }, 'Collaborators'),
-    cell: ({ row }) => {
-      return h(
-        'div',
-        { class: 'text-left font-medium' },
-        JSON.stringify(row.getValue('collaborators'))
-      )
-    }
-  }
-]
 </script>
 
 <template>
--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
 import { tasksWithProjectQuery } from '@/utils/supaQueries'
-import { RouterLink } from 'vue-router'
-import type { ColumnDef } from '@tanstack/vue-table'
 import type { TasksWithProjects } from '@/utils/supaQueries'
+import { columns } from '@/utils/tableColumns/tasksColumns'
 
 usePageStore().pageData.title = 'My Tasks'
 
@@ -18,64 +17,6 @@ const getTasks = async () => {
 }
 
 await getTasks()
-
-const columns: ColumnDef<TasksWithProjects[0]>[] = [
-  {
-    accessorKey: 'name',
-    header: () => h('div', { class: 'text-left' }, 'Name'),
-    cell: ({ row }) => {
-      return h(
-        RouterLink,
-        {
-          to: `/tasks/${row.original.id}`,
-          class: 'text-left font-medium hover:bg-muted block w-full'
-        },
-        () => row.getValue('name')
-      )
-    }
-  },
-  {
-    accessorKey: 'status',
-    header: () => h('div', { class: 'text-left' }, 'Status'),
-    cell: ({ row }) => {
-      return h('div', { class: 'text-left font-medium' }, row.getValue('status'))
-    }
-  },
-  {
-    accessorKey: 'due_date',
-    header: () => h('div', { class: 'text-left' }, 'Due date'),
-    cell: ({ row }) => {
-      return h('div', { class: 'text-left font-medium' }, row.getValue('due_date'))
-    }
-  },
-  {
-    accessorKey: 'projects',
-    header: () => h('div', { class: 'text-left' }, 'Project ID'),
-    cell: ({ row }) => {
-      return row.original.projects
-        ? h(
-            RouterLink,
-            {
-              to: `/projects/${row.original.projects.slug}`,
-              class: 'text-left font-medium hover:bg-muted block w-full'
-            },
-            () => row.original.projects?.name
-          )
-        : ''
-    }
-  },
-  {
-    accessorKey: 'collaborators',
-    header: () => h('div', { class: 'text-left' }, 'Collaborators'),
-    cell: ({ row }) => {
-      return h(
-        'div',
-        { class: 'text-left font-medium' },
-        JSON.stringify(row.getValue('collaborators'))
-      )
-    }
-  }
-]
 </script>
 
 <template>
