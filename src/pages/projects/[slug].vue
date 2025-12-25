cffe7dfc565a285b3f46c79abfe6abcfae23b046
Ana <ana.pereiraxc@gmail.com>
2025-12-25 17:53:29 +0000

    Update projects db schema. Add dynamic project info

--- src/pages/projects/[slug].vue
+++ src/pages/projects/[slug].vue
@@ -25,28 +25,16 @@ await getProject()
 </script>
 
 <template>
-  <Table>
+  <Table v-if="project">
     <TableRow>
       <TableHead> Name </TableHead>
-      <TableCell> {{ project?.name }} </TableCell>
+      <TableCell> {{ project.name }} </TableCell>
     </TableRow>
     <TableRow>
       <TableHead> Description </TableHead>
       <TableCell>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad iure qui tempora ex nihil, ab
-        reprehenderit dolorem sunt veritatis perferendis? Repudiandae quis velit quasi ab natus quia
-        ratione voluptas deserunt labore sed distinctio nam fuga fugit vero voluptates placeat
-        aperiam, saepe excepturi eos harum consectetur doloremque perspiciatis nesciunt! Incidunt,
-        modi.
+        {{ project.description }}
       </TableCell>
-    </TableRow>
-    <TableRow>
-      <TableHead> Assignee </TableHead>
-      <TableCell>Lorem ipsum</TableCell>
-    </TableRow>
-    <TableRow>
-      <TableHead> Project </TableHead>
-      <TableCell> Lorem ipsum. </TableCell>
     </TableRow>
     <TableRow>
       <TableHead> Status </TableHead>
@@ -58,8 +46,8 @@ await getProject()
         <div class="flex">
           <Avatar
             class="-mr-4 border border-primary hover:scale-110 transition-transform"
-            v-for="n in project?.collaborators.length"
-            :key="n"
+            v-for="colab in project.collaborators"
+            :key="colab"
           >
             <RouterLink class="w-full h-full flex items-center justify-center" to="">
               <AvatarImage src="" alt="" />
@@ -69,34 +57,40 @@ await getProject()
         </div>
       </TableCell>
     </TableRow>
-    <TableRow class="hover:bg-transparent">
-      <TableHead class="align-top pt-4"> Comments </TableHead>
-
-      <TableCell>
-        Comments cards goes in here..
-
-        <div class="flex flex-col justify-between p-3 bg-muted my-2 rounded-md">
-          <textarea
-            placeholder="Add your comment.."
-            class="w-full max-w-full overflow-y-auto prose-sm prose border rounded dark:prose-invert hover:border-muted bg-background border-muted p-3"
-          >
-          </textarea>
-          <div class="flex justify-between mt-3">
-            <Button> Comment </Button>
-            <div class="flex gap-4">
-              <button variant="ghost" @click.prevent>
-                <iconify-icon icon="lucide:paperclip"></iconify-icon>
-                <span class="sr-only">Attach file</span>
-              </button>
-              <button variant="ghost" @click.prevent>
-                <iconify-icon icon="lucide:image-up"></iconify-icon>
-
-                <span class="sr-only">Upload image</span>
-              </button>
-            </div>
-          </div>
-        </div>
-      </TableCell>
-    </TableRow>
   </Table>
+
+  <section v-if="project" class="mt-10 flex flex-col md:flex-row gap-5 justify-between grow">
+    <div class="flex-1">
+      <h2>Tasks</h2>
+      <div class="table-container">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Name</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Due Date</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow v-for="task in project.tasks" :key="task.id">
+              <TableCell>
+                <RouterLink :to="`/tasks/${task.id}`">{{ task.name }}</RouterLink>
+              </TableCell>
+              <TableCell>{{ task.status }}</TableCell>
+              <TableCell>{{ task.due_date }}</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+
+    <div class="flex-1">
+      <h2>Documents</h2>
+      <div class="table-container">
+        <p class="text-muted-foreground text-sm font-semibold px-4 py-3">
+          This project doemsn't have documents yet...
+        </p>
+      </div>
+    </div>
+  </section>
 </template>
