5adeb653f4ac8ed728d28d0750baabe9f2ee36c1
Ana <ana.pereiraxc@gmail.com>
2026-01-01 23:36:38 +0000

    Use browser local storage to persist session

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
 import AppErrorPage from '@/components/AppError/AppErrorPage.vue'
+import { supabase } from '@/lib/supabaseClient'
+
 const errorStore = useErrorStore()
+const authStore = useAuthStore()
 const { activeError } = storeToRefs(errorStore)
 
 onErrorCaptured((error) => {
   errorStore.setError({ error })
+})
+
+onMounted(async () => {
+  const { data } = await supabase.auth.getSession()
+
+  if (data.session?.user) await authStore.setAuth(data.session)
 })
 </script>
 
